146286053915bcb0915acf713cf7b5791c610421
Harsharan Singh Gill <91721566+Harsharan0212@users.noreply.github.com>
2023-07-13 02:49:01 +0530

    Add files via upload

---
+++ TempConversion.cpp
@@ -0,0 +1,12 @@
+// Harsharan Singh Gill   102106096  2EC5   PL2
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float C,F;
+    cout<<"Enter the temperature in degrees: "<<endl;
+    cin>>C;
+    F=(9*C/5)+32;
+    cout<<"Temperature in fahrenheit is: "<<F<<endl;
+}
